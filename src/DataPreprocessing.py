9243515cd028d0ec8f3addbf89ff2147759af076
jaroslav.veverka <jaroslav.veverka@icloud.com>
2020-10-21 18:56:02 +0200

    exploratory analysis

--- src/DataPreprocessing.py
+++ src/DataPreprocessing.py
@@ -6,11 +6,22 @@ source_path = '../data/UniversalBank.csv'
 # load csv dataset
 data = pd.read_csv(source_path)
 
+# any missing values?
 print(data.isnull().sum())
 print(data.head(10))
+
+# list of variable names
 print(list(data.columns))
 
-#some plots
+# create sub-dataframe
+print(data.loc[:, ['CreditCard']])
+print(data[['CreditCard']])
+
+# exploring of dependent variable
+rows_number = len(data)
+target_count = sum(data['CreditCard'] == 1)
+print(target_count/rows_number)
+
 plt.hist(data['CreditCard'], bins=2, rwidth=0.85)
 
 plt.title('Histogram of Credit Card variable')
